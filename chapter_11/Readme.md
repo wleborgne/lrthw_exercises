8d6d8cae4e1de662694176e685b13585111cf9e5
Bill LeBorgne <bill@leborgne.name>
2015-02-16 13:51:17 -0700

    Chapter 11 code and Readme

---
+++ chapter_11/Readme.md
@@ -0,0 +1,9 @@
+## Rubocop
+
+no issues
+
+## STUDY DRILLS
+1.  With no parameter supplied to `chomp`, `gets.chomp` will read a line from standard input and return that line as a 
+with the newline char(s) removed from the end.
+2.  
+3. see `quest.rb`
---
+++ chapter_11/quest.rb
@@ -0,0 +1,8 @@
+print 'What is your name? '
+name = gets.chomp
+print 'What is your quest? '
+quest = gets.chomp
+print 'What is the air-speed velocity of an unladen swallow? '
+answer = gets.chomp
+puts "Your name is #{name}, and your quest is #{quest}."
+puts "But '#{answer}'? WRONG!!!"
